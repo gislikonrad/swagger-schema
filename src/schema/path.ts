90419fbc1790550cca2bf241de70fd14ae6babc1
Gísli K. Björnsson <Gisli.K.Bjornsson@landsbankinn.is>
2017-08-28 14:44:03 +0000

    Makes properties of Path optional

--- src/schema/path.ts
+++ src/schema/path.ts
@@ -3,12 +3,12 @@ import { Reference } from "./reference";
 import { Parameter } from "./parameter";
 
 export class Path {
-  get: Operation;
-  put: Operation;
-  post: Operation;
-  delete: Operation;
-  options: Operation;
-  head: Operation;
-  patch: Operation;
-  parameters: (Reference|Parameter)[];
+  get?: Operation;
+  put?: Operation;
+  post?: Operation;
+  delete?: Operation;
+  options?: Operation;
+  head?: Operation;
+  patch?: Operation;
+  parameters?: (Reference|Parameter)[];
 }
