85c83cd700e784c5818135d9401ccbe6a2c81da9
Gísli K. Björnsson <Gisli.K.Bjornsson@landsbankinn.is>
2017-09-04 10:18:58 +0000

    Adds annotations to Contact object

--- src/schema/contact.ts
+++ src/schema/contact.ts
@@ -1,5 +1,11 @@
+/** Contact information for the exposed API. */
 export class Contact {
+  /** The identifying name of the contact person/organization. */
   name: string;
+
+  /** The URL pointing to the contact information. MUST be in the format of a URL. */
   url: string;
+
+  /** The email address of the contact person/organization. MUST be in the format of an email address. */
   email: string;
 }
