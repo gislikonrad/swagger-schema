4b0c1aed3a27c3cbfe92fc7d15c3daf09d7390e7
Gísli K. Björnsson <Gisli.K.Bjornsson@landsbankinn.is>
2017-09-04 11:05:02 +0000

    Adds annotations to the ExternalDocumentation object

--- src/schema/external-documentation.ts
+++ src/schema/external-documentation.ts
@@ -1,4 +1,7 @@
+/** Allows referencing an external resource for extended documentation. */
 export class ExternalDocumentation {
-  description: string;
+  /** A short description of the target documentation. GFM syntax can be used for rich text representation. */
+  description?: string;
+  /** The URL for the target documentation. Value MUST be in the format of a URL. */
   url: string;
 }
