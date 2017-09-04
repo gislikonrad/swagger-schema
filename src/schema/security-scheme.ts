2ee4f32851dd5576769e198c30b962ed49363666
Gísli K. Björnsson <Gisli.K.Bjornsson@landsbankinn.is>
2017-09-04 16:18:50 +0000

    Adds annotations for SecurityScheme

--- src/schema/security-scheme.ts
+++ src/schema/security-scheme.ts
@@ -1,12 +1,21 @@
 import { SecuritySchemeType } from "src";
 
+/** Allows the definition of a security scheme that can be used by the operations. Supported schemes are basic authentication, an API key (either as a header or as a query parameter) and OAuth2's common flows (implicit, password, application and access code). */
 export class SecurityScheme {
+  /** The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2". */
   type: SecuritySchemeType;
-  description: string;
-  name: string;
-  in: string;
-  flow: string;
-  authorizationUrl: string;
-  tokenUrl: string;
-  scopes: { [id: string]: string };
+  /** A short description for security scheme. */
+  description?: string;
+  /** The name of the header or query parameter to be used. */
+  name?: string;
+  /** The location of the API key. Valid values are "query" or "header". */
+  in?: string;
+  /** The flow used by the OAuth2 security scheme. Valid values are "implicit", "password", "application" or "accessCode". */
+  flow?: string;
+  /** The authorization URL to be used for this flow. This SHOULD be in the form of a URL. */
+  authorizationUrl?: string;
+  /** The token URL to be used for this flow. This SHOULD be in the form of a URL. */
+  tokenUrl?: string;
+  /** The available scopes for the OAuth2 security scheme. */
+  scopes?: { [id: string]: string };
 }
