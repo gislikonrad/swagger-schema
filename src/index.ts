5ca56be7222d69f65ed0ac95d237fa5c10f198e5
Gísli K. Björnsson <Gisli.K.Bjornsson@landsbankinn.is>
2017-09-04 10:43:46 +0000

    Finishes removal of Server and ServerVariables

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,8 @@ import { SecurityScheme } from "./schema/security-scheme";
 import { Swagger } from "./schema/swagger";
 import { Type } from "./schema/type";
 import { Types } from "./schema/types";
-import { Server } from './schema/server';
-import { ServerVariable } from "src/schema/server-variable";
+// import { Server } from './schema/server';
+// import { ServerVariable } from "src/schema/server-variable";
 
 export {
   CollectionFormat, 
@@ -47,8 +47,8 @@ export {
   SecuritySchemeType,
   SecuritySchemeTypes,
   SecurityScheme,
-  Server,
-  ServerVariable,
+  // Server,
+  // ServerVariable,
   Swagger,
   Type,
   Types
--- src/schema/swagger.ts
+++ src/schema/swagger.ts
@@ -4,16 +4,26 @@ import { Schema } from "./schema";
 import { SecurityScheme } from "./security-scheme";
 import { Server } from "./server";
 
+/** This is the root document object for the API specification. It combines what previously was the Resource Listing and API Declaration (version 1.2 and earlier) together into one document. */
 export class Swagger {
+  /** Specifies the Swagger Specification version being used. It can be used by the Swagger UI and other clients to interpret the API listing. The value MUST be "2.0". */
   swagger: string;
+  /** Provides metadata about the API. The metadata can be used by the clients if needed. */
   info: Info;
-  servers?: Server[];
+  /** The host (name or ip) serving the API. This MUST be the host only and does not include the scheme nor sub-paths. It MAY include a port. If the host is not included, the host serving the documentation is to be used (including the port). The host does not support path templating. */
   host: string;
+  /** The base path on which the API is served, which is relative to the host. If it is not included, the API is served directly under the host. The value MUST start with a leading slash (/). The basePath does not support path templating. */
   basePath: string;
+  /** The transfer protocol of the API. Values MUST be from the list: "http", "https", "ws", "wss". If the schemes is not included, the default scheme to be used is the one used to access the Swagger definition itself. */
   schemes: string[];
+  /** A list of MIME types the APIs can consume. This is global to all APIs but can be overridden on specific API calls. Value MUST be as described under Mime Types. */
   consumes: string[];
+  /** A list of MIME types the APIs can produce. This is global to all APIs but can be overridden on specific API calls. Value MUST be as described under Mime Types. */
   produces: string[];
+  /** The available paths and operations for the API. */
   paths: { [id: string] : Path };
+  /** An object to hold data types produced and consumed by operations. */
   definitions: { [id: string]: Schema };
+  /** Security scheme definitions that can be used across the specification. */
   securityDefinitions: { [id: string]: SecurityScheme };
 }
