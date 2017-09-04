fc08edeace530d6f44ab73c549b9537abed49b0a
Gísli K. Björnsson <Gisli.K.Bjornsson@landsbankinn.is>
2017-09-04 10:16:27 +0000

    Added annotations to Info object

--- src/schema/info.ts
+++ src/schema/info.ts
@@ -1,11 +1,23 @@
 import { Contact } from "./contact";
 import { License } from "./license";
 
+/** The object provides metadata about the API. The metadata MAY be used by the clients if needed, and MAY be presented in editing or documentation generation tools for convenience. */
 export class Info {
+  /** The title of the application */
   title: string;
+
+  /** A short description of the application. CommonMark syntax MAY be used for rich text representation. */
   description?: string;
+
+  /** A URL to the Terms of Service for the API. MUST be in the format of a URL. */
   termsOfService?: string;
+
+  /** The contact information for the exposed API. */
   contact?: Contact;
+
+  /** The license information for the exposed API. */
   licence?: License;
+
+  /** The version of the OpenAPI document (which is distinct from the OpenAPI Specification version or the API implementation version). */
   version: string;
 }
