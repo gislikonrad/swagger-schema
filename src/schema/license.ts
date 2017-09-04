dcab0fa068d77a71244aa86bccf66594d8c89568
Gísli K. Björnsson <Gisli.K.Bjornsson@landsbankinn.is>
2017-09-04 10:21:30 +0000

    Add annotations to License object

--- src/schema/license.ts
+++ src/schema/license.ts
@@ -1,4 +1,7 @@
+//** License information for the exposed API. */
 export class License {
+  /** The license name used for the API. */
   name: string;
+  /** A URL to the license used for the API. MUST be in the format of a URL. */
   url: string;
 }
