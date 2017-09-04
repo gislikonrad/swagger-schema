309a3ca55930e6abd136b4c38721e200a996e0d0
Gísli K. Björnsson <Gisli.K.Bjornsson@landsbankinn.is>
2017-09-04 10:43:15 +0000

    Changes comments to be the 2.0 spec comments

--- src/schema/info.ts
+++ src/schema/info.ts
@@ -1,18 +1,18 @@
 import { Contact } from "./contact";
 import { License } from "./license";
 
-/** The object provides metadata about the API. The metadata MAY be used by the clients if needed, and MAY be presented in editing or documentation generation tools for convenience. */
+/** The object provides metadata about the API. The metadata can be used by the clients if needed, and can be presented in the Swagger-UI for convenience. */
 export class Info {
   /** The title of the application */
   title: string;
-  /** A short description of the application. CommonMark syntax MAY be used for rich text representation. */
+  /** A short description of the application. GFM syntax can be used for rich text representation. */
   description?: string;
-  /** A URL to the Terms of Service for the API. MUST be in the format of a URL. */
+  /** The Terms of Service for the API. */
   termsOfService?: string;
   /** The contact information for the exposed API. */
   contact?: Contact;
   /** The license information for the exposed API. */
   licence?: License;
-  /** The version of the OpenAPI document (which is distinct from the OpenAPI Specification version or the API implementation version). */
+  /** Provides the version of the application API (not to be confused with the specification version). */
   version: string;
 }
